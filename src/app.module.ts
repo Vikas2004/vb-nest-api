b0e05bc4a009a7d901a6bdfc258315eb4a4d9f7b
Vikas2004 <S538336@NWMISSOURI.EDU>
2021-02-03 03:56:38 -0600

    user resource add, .env-example add

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { QuestModule } from './quest/quest.module';
 import { TeamModule } from './team/team.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [QuestModule, TeamModule],
+  imports: [QuestModule, TeamModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
